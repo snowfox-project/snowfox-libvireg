de73b6c6034f2bad1ef990a1ad56becc7e644f5e
Alexander Entinger <cto@lxrobotics.com>
2020-03-09 09:06:16 +0100

    Updating copyright notice to 2020

--- include/vireg/VirtualRegisterMap.hpp
+++ include/vireg/VirtualRegisterMap.hpp
@@ -1,6 +1,6 @@
 /**
  * Snowfox is a modular RTOS with extensive IO support.
- * Copyright (C) 2017 - 2019 Alexander Entinger / LXRobotics GmbH
+ * Copyright (C) 2017 - 2020 Alexander Entinger / LXRobotics GmbH
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
